29bb25bb83045d38b55a47e3e1e70c5f7aa75717
Diego Castaño <dcastano@hugeinc.com>
2016-08-09 16:53:42 -0500

    Initial Commit

---
+++ dollarbillconverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dollarbillconverter
+//
+//  Created by Diego Castaño on 8/9/16.
+//  Copyright © 2016 Diego Castaño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
